2717827bffc5e3244e341aa1b34e2b4fd1319054
Matthew Grainger <matthew.grainger@nina.no>
2019-11-25 12:40:18 +0100

    update 25/11/2019

--- Flowchart.R
+++ Flowchart.R
@@ -63,3 +63,63 @@ digraph boxes_and_circles {
       [5]: 'Research waste'
       [6]: 'Evidence synthesis'
 ")
+
+
+grViz("
+digraph boxes_and_circles {
+
+  # a 'graph' statement
+  graph [overlap = true, fontsize = 10]
+
+  # several 'node' statements
+  node [shape = box,
+        fontname = Helvetica]
+      tab1 [label = '@@1',color = blue]
+      tab2 [label = '@@2',color = blue]
+      tab3 [label = '@@3',color = blue]
+      tab4 [label = '@@4',color = blue]
+      tab6 [label = '@@6',color = blue]
+      tab8 [label = '@@8',color = red]
+      tab9 [label = '@@9',color = red]
+      tab10[label = '@@10',color = red]
+      tab11[label = '@@11',color = red]
+      
+  node [shape = circle,
+        fixedsize = true,
+        width = 2, color=blue] // sets as circles
+    tab7 [label = '@@7']
+    
+    node [shape = circle,
+        fixedsize = true,
+        width = 2, color=red] // sets as circles
+    tab5 [label = '@@5']
+  
+    
+    # several 'edge' statements
+ tab1->tab2->tab3->tab4->tab6->tab7 [color=blue]
+ tab1->tab8 [color=red,style=dashed]
+  tab2->tab9 [color=red,style=dashed]
+   tab3->tab10 [color=red,style=dashed]
+    tab4->tab11 [color=red,style=dashed]
+    tab8->tab5[color=red,style=dashed]
+    tab9->tab5[color=red,style=dashed]
+    tab10->tab5[color=red,style=dashed]
+    tab11->tab5[color=red,style=dashed]
+    tab6->tab1 [style=dashed]
+        tab6->tab2 [style=dashed]
+    tab6->tab5 [color=red, style=dashed]
+        
+ }
+      [1]: 'Questions relevant to stakeholders'
+      [2]: 'Appropriate design & methods'
+      [3]: 'Accessible full publication'
+      [4]: 'Unbiased reporting'
+      [5]: 'Wasteful research'
+      [6]: 'Evidence synthesis'
+      [7]: 'Utilisable research' 
+      [8]: 'Previous knowledge not accounted for'
+      [9]: 'Poor design; \\nquestionable research practices'
+      [10]: 'Publications not available to practitioners \\n& decision makers'
+      [11]: 'Lack of transparency'
+  ", height=1000)
+
