28b4f52c79291ac46a8eb9a64a5c75e811d6ca30
Matthew Grainger <matthew.grainger@nina.no>
2020-01-27 14:01:45 +0100

    2nd round review update 2

--- Flowchart.R
+++ Flowchart.R
@@ -82,8 +82,8 @@ digraph boxes_and_circles {
       tab6 [label = '@@6',color = blue]
       tab8 [label = '@@8',color = red]
       tab9 [label = '@@9',color = red]
-      tab10[label = '@@10',color = red]
-      tab11[label = '@@11',color = red]
+      tab10 [label = '@@10',color = red]
+      tab11 [label = '@@11',color = red]
       
   node [shape = circle,
         fixedsize = true,
@@ -123,3 +123,72 @@ digraph boxes_and_circles {
       [10]: 'Lack of transparency'
       [11]: 'Publications not available to practitioners \\n& decision makers'
   ", height=1000)
+
+
+
+grViz("
+    digraph boxes_and_circles {
+    # a 'graph' statement
+    graph [overlap = true, fontsize = 10]
+    
+    # left 'node' statements
+    node [shape = box, fontname = Helvetica]
+      tab1 [label = '@@1',color = blue]
+      tab2 [label = '@@2',color = blue]
+      tab3 [label = '@@3',color = blue]
+      tab4 [label = '@@4',color = blue]
+      tab6 [label = '@@6',color = blue]
+    
+    # right node statements
+      tab8 [label = '@@8',color = red]
+      tab9 [label = '@@9',color = red]
+      tab10 [label = 'Lack of transparency',color = red]
+      tab11 [label = 'Publications not available to practitioners \\n& decision makers',color = red]
+      
+      node [shape = circle,fixedsize = true,
+        width = 2, color=blue] // sets as circles
+    tab7 [label = '@@7']
+    
+    node [shape = circle,
+        fixedsize = true,
+        width = 2, color=red] // sets as circles
+    tab5 [label = '@@5']
+
+        # several 'edge' statements
+ tab1->tab2->tab3->tab4->tab6->tab7 [color=blue]
+ tab1->tab8 [color=red,style=dashed]
+  tab2->tab9 [color=red,style=dashed]
+   tab3->tab10 [color=red,style=dashed]
+    tab4->tab11 [color=red,style=dashed]
+    tab8->tab5 [color=red,style=dashed]
+    tab9->tab5 [color=red,style=dashed]
+    tab10->tab5 [color=red,style=dashed]
+    tab11->tab5 [color=red,style=dashed]
+    tab6->tab1 [style=dashed]
+        tab6->tab2 [style=dashed]
+    tab6->tab5 [color=red, style=dashed]
+    
+    tab8->tab9[style=invis]
+    tab9->tab10[style=invis]
+    tab10->tab11[style=invis]
+        
+        # define ranks
+    
+         subgraph {
+            rank = same; tab6; tab11
+         }
+         
+        
+    }
+       [1]: 'Questions are relevant to stakeholders'
+      [2]: 'Appropriate design & methods used'
+      [3]: 'Unbiased reporting'
+      [4]: 'Accessible full publication'
+      [5]: 'Wasteful research'
+      [6]: 'Evidence synthesis'
+      [7]: 'Utilisable research' 
+      [8]: 'Previous knowledge not accounted for'
+      [9]: 'Poor design; \\nquestionable research practices'
+        ")
+#, height=1000)
+
